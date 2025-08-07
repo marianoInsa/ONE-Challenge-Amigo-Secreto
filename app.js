6663a63454fe7d7516aeb4ac5d9d4f408927f06e
marianoInsa <marianoinsaurralde5@gmail.com>
2025-08-07 17:13:37 -0300

    add función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let amigos = [];
 
+// funcion para agregar amigos
 function agregarAmigo() {
   let amigo = document.getElementById("amigo").value;
 
@@ -13,5 +14,19 @@ function agregarAmigo() {
     amigos.push(amigo);
     console.log(`Lista de amigos: ${amigos}`);
     document.getElementById("amigo").value = "";
+    mostrarAmigos();
+  }
+}
+
+// funcion para mostrar lista de amigos
+function mostrarAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let amigoLista = document.createElement("li");
+    amigoLista.innerHTML = amigos[i];
+    listaAmigos.appendChild(amigoLista);
   }
 }
